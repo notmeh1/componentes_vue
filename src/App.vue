9fcdb5482607bb8bdab0cb9e2b47598e7711bf31
Andres <zleckzxd@gmail.com>
2021-08-19 00:05:27 -0400

    base vue & css

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="App">
+    <Personajes></Personajes>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import Personajes from "./components/Personajes";
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
+  name: "app",
+  components: { Personajes },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background-image: url(https://rickandmortypod.com/wp-content/uploads/2018/11/cropped-RM_page-header_background1-3.png);
+  font-family: sans-serif;
 }
 </style>
---
+++ src/components/Personajes.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <ul>
+      <li v-for="(personaje, index) in personajes" :key="index" class="card_list">
+        <img :src="personaje.src" alt="" class="card_img"/>
+        <h1 class="card_name">{{personaje.nombre}}</h1>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Personajes",
+  data: () => ({
+    personajes: [
+      {
+        nombre: "Rick Sanchez",
+        src: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+      },
+      {
+        nombre: "Morty Smith",
+        src: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+      },
+    ],
+  }),
+};
+</script>
+
+<style lang="scss">
+.container {
+  display: flex;
+}
+.card_list {
+  flex-direction: column;
+}
+.card_img {
+  border-radius: 150px;
+}
+.card_name {
+  text-align: center;
+  color: #ffffff;
+}
+</style>
